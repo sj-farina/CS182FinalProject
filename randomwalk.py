8fd8889a6ebf31323c6f8537ec7a8773600df706
Janey <janey.farina@gmail.com>
2016-11-29 14:03:41 -0500

    fixed portfolio and reward function

--- randomwalk.py
+++ randomwalk.py
@@ -14,7 +14,12 @@ import random as rand
 
 # initilize the starting number of stocks and the starting bank ballance
 stocks_held = 100
-current_ballance = [0]
+bank_ballance = 100
+portfolio = []
+
+# testing paramaters to be tuned by user
+MAX_SELL = 50
+MIN_SELL = 1
 
 
 
@@ -36,31 +41,34 @@ def loadData(file):
 	return np.genfromtxt(file, delimiter=',', skip_header=1,
             skip_footer=1, names=['date', 'open', 'high', 'low', 'close', 'adj'])
 
-# buys num_to_buy number of stocks and updatets bank ballance accordingly, debt is allowed
-def buy(time_index, num_to_buy = 1):
-	global stocks_held
-	current_ballance.append(current_ballance[time_index] - stock['open'][time_index] * num_to_buy)
-	stocks_held += num_to_buy
+# buys num_to_trade number of stocks and updatets bank ballance accordingly, debt is allowed
+def buy(time_index, num_to_trade = 1):
+	global stocks_held, bank_ballance
+	stocks_held += num_to_trade
+	bank_ballance -= num_to_trade*stock['open'][time_index]
+	portfolio.append(stocks_held*stock['open'][time_index] + bank_ballance)
 
-# sells num_to_sell number of stocks and updatets bank ballance accordingly, 
+
+# sells num_to_trade number of stocks and updatets bank ballance accordingly, 
 # selling more than you own is not permitted
-def sell(time_index, num_to_sell = 1):
-	global stocks_held
-	# you can't sell more stocks than you have, hold instead
+def sell(time_index, num_to_trade = 1):
+	global stocks_held, bank_ballance
+
 	if stocks_held <= 0:
 		hold(time_index)
 	else:
-		# if selling ammount would make us negative, just sell everything
-		if ((stocks_held - num_to_sell) <= 0): 
-			current_ballance.append(current_ballance[time_index] + stock['open'][time_index] * stocks_held)
+		if ((stocks_held - num_to_trade) <= 0): 
+			bank_ballance += stocks_held*stock['open'][time_index]
 			stocks_held = 0
 		else:
-			current_ballance.append(current_ballance[time_index] + stock['open'][time_index] * num_to_sell)
-			stocks_held -= num_to_sell
+			stocks_held -= num_to_trade
+			bank_ballance += num_to_trade*stock['open'][time_index]
+		portfolio.append(stocks_held*stock['open'][time_index] + bank_ballance)
+
+
 # appends the same value to ballance
 def hold(time_index):
-	current_ballance.append(current_ballance[time_index])
-
+	portfolio.append(stocks_held*stock['open'][time_index] + bank_ballance)
 
 
 ###########
@@ -73,19 +81,22 @@ stock = loadData('BA_2Y_14_15.csv')
 
 for i in range(len(stock['open'])):
 	rand_num = rand.randint(0,2)
+	# rand_to_trade = 1
+	rand_to_trade = rand.randint(MIN_SELL, MAX_SELL)
 	#wtf? why does python not have switch statements?
 	if rand_num == 0:
 		print 'sell'
-		sell(i)
+		sell(i, rand_to_trade)
 	elif rand_num == 1:
 		print 'hold'
+		print stocks_held
 		hold(i)
 	elif rand_num == 2:
 		print 'buy'
-		buy(i)
+		buy(i, rand_to_trade)
 
-print current_ballance
-plotData(stock['open'], current_ballance)
+# print portfolio
+plotData(stock['open'], portfolio)
 
 
 

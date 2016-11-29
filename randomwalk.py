657505ab3cb1741004c224ad042d507a8042ccb7
Janey <janey.farina@gmail.com>
2016-11-29 14:56:43 -0500

    redid the visualization to make is run multiple trials and have pretty colors

--- randomwalk.py
+++ randomwalk.py
@@ -13,28 +13,15 @@ import random as rand
 
 
 # initilize the starting number of stocks and the starting bank ballance
-stocks_held = 100
-bank_ballance = 100
-portfolio = []
+START_BANK = 100
+START_STOCK = 100
+
 
 # testing paramaters to be tuned by user
 MAX_SELL = 50
 MIN_SELL = 1
+TRIALS = 50
 
-
-
-# plot the stock prices to compare to your bank balance (random walk)
-def plotData(stock, ballance):
-	fig = plt.figure()
-	ax1 = fig.add_subplot(211)
-	ax2 = fig.add_subplot(212)
-	ax1.set_title("Title")    
-	ax1.set_xlabel('time')
-	ax1.set_ylabel('price')
-	ax1.plot(range(len(stock)), stock, color='r', label='stock price')
-	ax2.plot(range(len(ballance)), ballance, color='b', label='ballance')
-	leg = ax1.legend()
-	plt.show()
 
 # load the training file
 def loadData(file):
@@ -71,6 +58,30 @@ def hold(time_index):
 	portfolio.append(stocks_held*stock['open'][time_index] + bank_ballance)
 
 
+def randomWalk(stock):
+	global stocks_held, bank_ballance, portfolio
+	stocks_held = START_STOCK
+	bank_ballance = START_BANK
+	portfolio = []
+
+	for i in range(len(stock['open'])):
+		rand_num = rand.randint(0,2)
+		# rand_to_trade = 1
+		rand_to_trade = rand.randint(MIN_SELL, MAX_SELL)
+		#wtf? why does python not have switch statements?
+		if rand_num == 0:
+			print 'sell'
+			sell(i, rand_to_trade)
+		elif rand_num == 1:
+			print 'hold'
+			print stocks_held
+			hold(i)
+		elif rand_num == 2:
+			print 'buy'
+			buy(i, rand_to_trade)
+
+	return portfolio
+
 ###########
 # Main code
 ###########
@@ -79,24 +90,26 @@ def hold(time_index):
 # pick the data file to read from
 stock = loadData('BA_2Y_14_15.csv')
 
-for i in range(len(stock['open'])):
-	rand_num = rand.randint(0,2)
-	# rand_to_trade = 1
-	rand_to_trade = rand.randint(MIN_SELL, MAX_SELL)
-	#wtf? why does python not have switch statements?
-	if rand_num == 0:
-		print 'sell'
-		sell(i, rand_to_trade)
-	elif rand_num == 1:
-		print 'hold'
-		print stocks_held
-		hold(i)
-	elif rand_num == 2:
-		print 'buy'
-		buy(i, rand_to_trade)
+
 
 # print portfolio
-plotData(stock['open'], portfolio)
+# plotData(stock['open'], portfolio)
 
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+ax1.set_title("Stock Value")    
+ax1.set_xlabel('time')
+ax1.set_ylabel('price')
+ax2.set_title("Portfolio Value")    
+ax2.set_xlabel('time')
+ax2.set_ylabel('value')
+ax1.plot(range(len(stock['open'])), stock['open'], color='r', label='Stock Price')
 
+for each in range(TRIALS):
+	rand_port = randomWalk(stock)
+	ax2.plot(range(len(rand_port)), rand_port, label='Portfolio Value')
+
+leg = ax1.legend()
+plt.show()
 

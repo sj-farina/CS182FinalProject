9a3411ac1416adbe7069e1b20659dfaeeccc68d8
Janey <janey.farina@gmail.com>
2016-12-01 22:04:32 -0500

    basic q-agent working, needs tuning

--- qLearningAgent.py
+++ qLearningAgent.py
@@ -14,12 +14,16 @@ import matplotlib.collections  as mc
 import random as rd
 import math, collections, sys
 
+# Are you training or testing???
+TRAINING = 1
+TESTING = 0
+
 # Pick the file to read from
-INFILE = 'BA_6M_15.csv'
+# INFILE = 'BA_6M_15.csv'
 # INFILE = 'BA_1Y_15.csv'
 # INFILE = 'BA_2Y_14_15.csv'
 # INFILE = 'BA_5Y_11_15.csv'
-# INFILE = 'BA_15Y_01_15.csv'
+INFILE = 'BA_15Y_01_15.csv'
 
 
 # Initialize the starting number of stocks and the starting bank balance
@@ -27,11 +31,11 @@ START_BANK = 100
 START_STOCK = 100
 
 # Training variables
-EPSILON = 1
-ALPHA = 1
-DISCOUNT = 1
+EPSILON = .3
+ALPHA = .5
+DISCOUNT = .5
 ITERATIONS = 10
-LOOKAHEAD = 5
+LOOKAHEAD = 50
 
 # Helpers and things
 stocks_held = START_STOCK
@@ -50,13 +54,17 @@ def loadData(file):
     return np.insert(close_value, np.arange(len(open_value)), open_value)
 
 
+########################################
+# Q LEARNING CODE 
+########################################
+
 # Find the slope between this point and the last
 def getShortTermTrend(cur_time):
-    # if this is the first data point, assume a slope of zero
-    if cur_time == 0:
-        return 0
+    # # if this is the first data point, assume a slope of zero
+    # if cur_time == 0:
+    #     return 0
     # Multiply by 100, set to int, equiv of truncating at 1 decimals
-    slope = int((data_set[cur_time] - data_set[cur_time-1]) * 10)
+    slope = int((data_set[cur_time] - data_set[cur_time - LOOKAHEAD]) * 100)
     # Cap -10 to 10 to limit state space
     if slope > 10:
         return 10
@@ -127,6 +135,42 @@ def update(cur_time, state, action, nextState, reward):
         DISCOUNT * getMaxStateValue(nextState, cur_time +1) - values[state,action])
 
 
+########################################
+# STOCK MANIPULATION CODE
+########################################
+
+# buys num_to_trade number of stocks and updates bank balance accordingly, debt is allowed
+def buy(cur_time, num_to_trade = 10):
+    global stocks_held, bank_ballance
+    stocks_held += num_to_trade
+    bank_ballance -= num_to_trade*data_set[cur_time]
+    portfolio.append(stocks_held*data_set[cur_time] + bank_ballance)
+
+
+# sells num_to_trade number of stocks and updates bank balance accordingly, 
+def sell(cur_time, num_to_trade = 10):
+    global stocks_held, bank_ballance
+    if ((stocks_held - num_to_trade) <= 0): 
+        bank_ballance += stocks_held*data_set[cur_time]
+        stocks_held = 0
+    else:
+        stocks_held -= num_to_trade
+        bank_ballance += num_to_trade*data_set[cur_time]
+    portfolio.append(stocks_held*data_set[cur_time] + bank_ballance)
+
+# appends the current value to balance
+def hold(cur_time):
+    portfolio.append(stocks_held*data_set[cur_time] + bank_ballance)
+
+
+def tradeStocks(cur_time, action):
+    if action == 'sell':
+        sell(cur_time)
+    elif action == 'hold':
+        hold(cur_time)
+    elif action == 'buy':
+        buy(cur_time)
+
 
 
 
@@ -136,24 +180,52 @@ def update(cur_time, state, action, nextState, reward):
 
 data_set = loadData(INFILE)
 values = collections.Counter()
-# How many times should we run this?
-for i in range(ITERATIONS):
-    # Iterates over array, time (cur_time) is arbitrary, two points per day
-    for cur_time in range(len(data_set) - LOOKAHEAD):
+if (TRAINING):
+    print 'Im training'
+    # How many times should we run this?
+    for i in range(ITERATIONS):
+        # Iterates over array, time (cur_time) is arbitrary, two points per day
+        for cur_time in range(LOOKAHEAD, len(data_set) - 2*LOOKAHEAD):
+            state = getShortTermTrend(cur_time)
+            nextState = getShortTermTrend(cur_time+1)
+            action = pickAction(state, cur_time)
+            reward = getReward(cur_time, action)
+            update(cur_time, state, action, nextState, reward)
+# TODO: Make this selectable from cmdline and save/load trained dataset elsewhere
+    TRAINING = 0
+    TESTING = 1
+    print values
+# INFILE = 'BA_6M_15.csv'
+# INFILE = 'BA_1Y_15.csv'
+INFILE = 'BA_2Y_14_15.csv'
+data_set = loadData(INFILE)
+
+
+if (TESTING):
+    print 'im testing'
+    stocks_held = START_STOCK
+    bank_ballance = START_BANK
+    portfolio = []
+    for cur_time in range(len(data_set)):
         state = getShortTermTrend(cur_time)
-        nextState = getShortTermTrend(cur_time+1)
-        action = pickAction(state, cur_time)
-        reward = getReward(cur_time, action)
-        update(cur_time, state, action, nextState, reward)
+        action = getBestAction(state, cur_time)
+        tradeStocks(cur_time, action)
+
+else:
+    print "What are you doing dude?"
 
 
 
 
-# # Optional plot for reference
-# fig = plt.figure()
-# ax1 = fig.add_subplot(111)
-# ax1.plot(range(len(data_set)), data_set, color='r', label='Stock Price')
-# plt.show()
+# Optional plot for reference
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+
+ax1.plot(range(len(data_set)), data_set, color='r', label='Stock Price')
+ax2.plot(range(len(portfolio)), portfolio, label='Portfolio Value')
+
+plt.show()
 
 
 
